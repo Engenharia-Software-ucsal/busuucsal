f37e0c357ef2b1e231d005ba566c8ece4ad856c8
luminuszz <luminuszz43@gmail.com>
2024-07-25 16:09:10 -0300

    fix
    Signed-off-by: luminuszz <luminuszz43@gmail.com>

--- components/container.tsx
+++ components/container.tsx
@@ -2,18 +2,31 @@ import React from "react";
 import { Box } from "@/components/ui/box";
 import { tva } from "@gluestack-ui/nativewind-utils/tva";
 import { VariantProps } from "@gluestack-ui/nativewind-utils";
+import { ScrollView } from "react-native";
 
 const baseStyle = tva({
-  base: "flex flex-col mt-20",
+  base: "flex flex-col mt-20 mb-40",
 });
 
 export interface ContainerProps extends VariantProps<typeof baseStyle> {
   children: React.ReactNode;
   className?: string;
+  useScrollView?: boolean;
 }
 
-export function Container({ children, className, ...props }: ContainerProps) {
-  return (
+export function Container({
+  children,
+  className,
+  useScrollView,
+  ...props
+}: ContainerProps) {
+  return useScrollView ? (
+    <ScrollView>
+      <Box {...props} className={baseStyle({ class: className })}>
+        {children}
+      </Box>
+    </ScrollView>
+  ) : (
     <Box {...props} className={baseStyle({ class: className })}>
       {children}
     </Box>
